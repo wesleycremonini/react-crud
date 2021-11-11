b5be97c0bbd1b4cc697898faa33196f099cb476c
wesleycremonini <wesley18.cre@gmail.com>
2021-11-10 21:01:21 -0300

    Seeting gh-pages

--- crud/src/index.js
+++ crud/src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
       <DBProvider>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <App />
         </BrowserRouter>
       </DBProvider>
